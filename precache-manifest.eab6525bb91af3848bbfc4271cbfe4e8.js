0621551ec5ece9ac594393aad47412d0b8d4a4c0
Beatriz Morera <beatrizmorera2016@gmail.com>
2020-01-28 11:59:14 +0100

    Updates

---
+++ precache-manifest.eab6525bb91af3848bbfc4271cbfe4e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090aaee99ddd6b1a99689ea421bdbba3",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "fd3491f59a7917ced649",
+    "url": "/todo-list/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "41fb92675ead90248399",
+    "url": "/todo-list/static/css/main.246cc85f.chunk.css"
+  },
+  {
+    "revision": "fd3491f59a7917ced649",
+    "url": "/todo-list/static/js/2.64eaa5ed.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/todo-list/static/js/2.64eaa5ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41fb92675ead90248399",
+    "url": "/todo-list/static/js/main.ae70604a.chunk.js"
+  },
+  {
+    "revision": "3c8bc7aa72310e7d0f4e",
+    "url": "/todo-list/static/js/runtime-main.901ffa1a.js"
+  },
+  {
+    "revision": "150214d855ea1ae1b84671d1e2cf4dbd",
+    "url": "/todo-list/static/media/image-desktop.150214d8.png"
+  },
+  {
+    "revision": "73fad95bd389d72cb94440a607ba8605",
+    "url": "/todo-list/static/media/image-mobile.73fad95b.png"
+  }
+]);
